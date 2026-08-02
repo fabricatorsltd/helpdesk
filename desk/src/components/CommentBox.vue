de152798af60ffc946ebc02ecb43c27a4affd79c
Pietro di Caprio <pietro@fabricators.ltd>
2026-08-02 17:04:21 +0000

    fix: translate the Discard button label

--- desk/src/components/CommentBox.vue
+++ desk/src/components/CommentBox.vue
@@ -68,7 +68,7 @@
             variant="solid"
           />
         </div>
-        <Button label="Discard" @click="handleDiscard" />
+        <Button :label="__('Discard')" @click="handleDiscard" />
       </div>
       <div
         class="flex flex-wrap gap-2"
