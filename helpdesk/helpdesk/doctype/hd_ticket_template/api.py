9711ac329221ec6986a1131cd8ef2de1f0a404e7
Pietro di Caprio <pietro@fabricators.ltd>
2026-08-06 09:44:50 +0000

    feat: hide non-writable fields from the customer ticket create form

--- helpdesk/helpdesk/doctype/hd_ticket_template/api.py
+++ helpdesk/helpdesk/doctype/hd_ticket_template/api.py
@@ -100,6 +100,7 @@ def get_fields(template: str, fetch: Literal["Custom Field", "DocField"]):
             QBFetch.link_filters,
             QBFetch.depends_on,
             QBFetch.mandatory_depends_on,
+            QBFetch.permlevel,
             fields.fieldname,
             fields.hide_from_customer,
             fields.required,
